dd10b2f57ea2baa493aa339672c690d34a58f45e
Rahul_Singh <koderrahul@gmail.com>
2024-07-20 05:38:45 +0530

    textchanges

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -30,8 +30,8 @@ export const Footer = () => {
                 </h2>
                 <ul className="text-gray-500 dark:text-gray-400 font-medium">
                   <li className="mb-4">
-                    <a href="https://flowbite.com/" className="hover:underline">
-                      Flowbite
+                    <a href="" className="hover:underline">
+                      Nextbase
                     </a>
                   </li>
                   <li>
@@ -90,7 +90,7 @@ export const Footer = () => {
           <div className="sm:flex sm:items-center sm:justify-between">
             <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
               © 2023{' '}
-              <a href="https://flowbite.com/" className="hover:underline">
+              <a href="" className="hover:underline">
                 NextBase™
               </a>
               . All Rights Reserved.
